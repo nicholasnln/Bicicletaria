6240219b41879f0e31d98d1bc6882c231fba8793
nicholasnln <nicholasnloiola@hotmail.com>
2025-05-18 20:18:11 -0300

    feat: add new page and improve of style

--- js/lista-produtos.js
+++ js/lista-produtos.js
@@ -23,12 +23,28 @@ document.addEventListener('DOMContentLoaded', () => {
         const card = document.createElement('div');
         card.classList.add('product-card');
 
-        card.innerHTML = `
-            <img src="${produto.imagem}" alt="Imagem do Produto">
-            <div class="product-description">${produto.descricao}</div>
-            <div class="product-price">R$ ${produto.valor.toFixed(2)}</div>
-            <button class="add-to-cart-btn">Adicionar ao Carrinho</button>
-        `;
+        const imagem = document.createElement('img');
+        imagem.src = produto.imagem;
+        imagem.alt = `Imagem de ${produto.descricao}`;
+        imagem.classList.add('product-image');
+
+        const descricao = document.createElement('div');
+        descricao.classList.add('product-description');
+        descricao.textContent = produto.descricao;
+
+        const preco = document.createElement('div');
+        preco.classList.add('product-price');
+        preco.textContent = `R$ ${produto.valor.toFixed(2)}`;
+
+        const botao = document.createElement('button');
+        botao.classList.add('add-to-cart-btn');
+        botao.textContent = 'Adicionar ao Carrinho';
+
+        // Monta o card
+        card.appendChild(imagem);
+        card.appendChild(descricao);
+        card.appendChild(preco);
+        card.appendChild(botao);
 
         lista.appendChild(card);
     });
